137219c710f510ab665f19ce4fb1e055c02512f5
joelgrod <joelgrod@gmail.com>
2021-08-23 19:25:44 +0100

    first commit - router

---
+++ src/infrastructure/shared/components/Navbar.vue
@@ -0,0 +1,26 @@
+<template>
+    <div class="nav-container">
+        <router-link class="nav-element" to="/id">Pokemon</router-link>
+        <router-link class="nav-element" to="/about">About</router-link>
+        <router-link class="nav-element" to="/">Home</router-link>
+    </div>
+</template>
+
+<style scoped>
+
+    .nav-container {
+        padding: 0 2rem;
+    }
+
+    .nav-container .nav-element {
+        font-weight: bold;
+        color: #2c3e50;
+        text-decoration: none;
+        padding: 0 1rem;
+    }
+
+    .nav-container .router-link-exact-active {
+        color: #42b983;
+    }
+
+</style>
---
+++ src/router/router.js
@@ -0,0 +1,30 @@
+import { createRouter, createWebHashHistory } from 'vue-router'
+
+// Pages (Lazy loaded)
+// Infrastructure Pages (Lazy loaded)
+
+const routes = [
+    { 
+        path: '/', 
+        component: () => import(/* webpackChunkName: "HomePage" */ '../domain/pokemon/pages/HomePage')
+    },
+    { 
+        path: '/about', 
+        component: () => import(/* webpackChunkName: "AboutPage" */ '../domain/pokemon/pages/AboutPage')
+    },
+    { 
+        path: '/id', 
+        component: () => import(/* webpackChunkName: "PokemonPage" */ '../domain/pokemon/pages/PokemonPage')
+    },
+    { 
+        path: '/:pathMatch(.*)*', 
+        component: () => import(/* webpackChunkName: "NotFoundPage" */ '../infrastructure/shared/pages/NotFoundPage')
+    }
+]
+
+const router = createRouter({
+    history: createWebHashHistory(),
+    routes
+})
+
+export default router
